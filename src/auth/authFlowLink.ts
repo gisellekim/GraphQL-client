90ea5fbae458f565714e4ae8e3567a97a06752a9
giselle <yajisk0@gmail.com>
2024-02-14 16:01:44 +0000

    chore: add comments on authFlowLink

--- src/auth/authFlowLink.ts
+++ src/auth/authFlowLink.ts
@@ -6,13 +6,17 @@ import { HttpLink, from } from "@apollo/client"
 import * as keytar from "keytar"
 import { getCode } from "./getCode"
 
+// this is where I am going to make all the requests
 const GITHUB_BASE_URL = "https://api.gitgub.com/graphql"
 
+// this is responsible for actually making the requests
 const httpLink = new HttpLink({ uri: GITHUB_BASE_URL })
 
+// cached storage for the user token
 let token: string | null
 let tokenInvalid = false
 
+// token middleware
 const withToken = setContext(async (_, { headers = {} }) => {
   if (token) return { token }
 
@@ -28,6 +32,7 @@ const withToken = setContext(async (_, { headers = {} }) => {
   return { token }
 })
 
+// auth bearer middleware
 const withAuthBearer = setContext(async (_, { headers = {}, token }) => {
   return {
     headers: {
@@ -37,7 +42,9 @@ const withAuthBearer = setContext(async (_, { headers = {}, token }) => {
   }
 })
 
+// reset token middleware -> engancing security
 const resetToken = onError(({ networkError }) => {
+  // when the user is  unauthorised or token is expired (it happens when user logged out or session is expired)
   if ((networkError as ServerError)?.statusCode === 401 && !!token) {
     token = null
     tokenInvalid = true
