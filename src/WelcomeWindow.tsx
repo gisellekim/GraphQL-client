66372c017c81d11412e000e1603c911294d597c1
giselle <yajisk0@gmail.com>
2024-02-15 23:30:24 +0000

    feat: updated WelcomeWindow layout using helper components

--- src/WelcomeWindow.tsx
+++ src/WelcomeWindow.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { useQuery, gql } from "@apollo/client"
+import { Text } from "./shared/Text"
+import { Panel } from "./shared/Panel"
 
 const GET_USER_INFO = gql`
   query getUserInfo {
@@ -20,5 +22,17 @@ type UserInfo = {
 export const WelcomeWindow = () => {
   const { loading, data } = useQuery<UserInfo>(GET_USER_INFO)
 
-  return <>{loading ? "Loading..." : JSON.stringify(data)}</>
+  return (
+    <Panel height={12} left="center" top="25%">
+      <Text left="center">Welcome to Github Manager</Text>
+      {loading ? (
+        <Text top={3}>Loading...</Text>
+      ) : (
+        <>
+          <Text top={3}>{`Name: ${data?.viewer.name}`}</Text>
+          <Text top={5} width={50}>{`Bio: ${data?.viewer.bio}`}</Text>
+        </>
+      )}
+    </Panel>
+  )
 }
