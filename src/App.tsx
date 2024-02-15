0e0826184f7ea11ffb06858d49684773f088cfb2
giselle <yajisk0@gmail.com>
2024-02-15 15:16:00 +0000

    feat: add the WelcomeWindow component get the user data with GraphQL queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from "react"
+import { WelcomeWindow } from "./WelcomeWindow"
 
 export const App = () => {
-  return <blessed-box style={{ bg: "#0000ff" }}>React Blessed</blessed-box>
+  return (
+    <blessed-box
+      style={{
+        bg: "#0000ff",
+      }}
+    >
+      <WelcomeWindow />
+    </blessed-box>
+  )
 }
